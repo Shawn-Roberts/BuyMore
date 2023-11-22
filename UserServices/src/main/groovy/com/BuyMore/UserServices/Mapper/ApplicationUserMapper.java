b75b41deeb054d73d574cc08f2dcb11c4c7de308
Shawn Roberts <Shawn.Roberts@grey.ca>
2023-11-21 22:32:32 -0500

    updated mapper controller

--- UserServices/src/main/groovy/com/BuyMore/UserServices/Mapper/ApplicationUserMapper.java
+++ UserServices/src/main/groovy/com/BuyMore/UserServices/Mapper/ApplicationUserMapper.java
@@ -3,7 +3,6 @@ package com.BuyMore.UserServices.Mapper;
 import com.BuyMore.UserServices.DTO.ApplicationUserDTO;
 import com.BuyMore.UserServices.Entity.ApplicationUser;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
